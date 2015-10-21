14352481b9f509e0bcd1957ef9bdc7b3592f2334
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-10-21 14:13:00 +0300

    something wrong with ui rouert

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -3,15 +3,13 @@ app.controller("MainCtrl", ['$scope', 'posts', function($scope, posts){
 	$scope.posts = posts.posts;
 
 	$scope.addPost = function(){
-		$scope.posts.push(
-			{
-				title: 	$scope.title,
-				link: 	$scope.link,
-				upvotes: 0,
-				comments: []
-			});
-		$scope.title = '';
-		$scope.link = '';
+	  if(!$scope.title || $scope.title === '') { return; }
+	  posts.create({
+	    title: $scope.title,
+	    link: $scope.link,
+	  });
+	  $scope.title = '';
+	  $scope.link = '';
 	};
 	$scope.incrementUpvotes = function(post){
 		post.upvotes += 1;
@@ -31,30 +29,61 @@ app.controller("PostsCtrl", ['$scope', '$stateParams', 'posts', function($scope,
 		$scope.body = '';
 	};
 
-	$scope.incrementUpvotes = function(comment){
-		comment.upvotes +=1;
+	$scope.incrementUpvotes = function(post) {
+	  posts.upvote(post);
 	};
 }]);
 
-app.factory("posts", function(){
+app.factory("posts", ["$http", function($http){
 	var o = {
 		posts: []
 	}
-	return o;
-});
+ 	o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+  o.create = function(post) {
+	  return $http.post('/posts', post).success(function(data){
+	    o.posts.push(data);
+	  });
+	};
+	o.upvote = function(post) {
+  	return $http.put('/posts/' + post._id + '/upvote')
+	    .success(function(data){
+	      post.upvotes += 1;
+	    });
+	};
+	o.get = function(id) {
+	  return $http.get('/posts/' + id).then(function(res){
+	    return res.data;
+	  });
+	};
+  return o;
+}]);
 
-app.config(["$stateProvider", "$urlRouterProvider", 
-	function($stateProvider, $urlRouterProvider){
-		$stateProvider
-			.state('home', {
-				url: '/home',
-				templateUrl: '/home.html',
-				controller: 'MainCtrl'
-			});
-		$urlRouterProvider.otherwise('/home');
-		$stateProvider.state('post', {
-			url: '/posts/{id}',
-			templateUrl: '/posts.html',
-			controller: 'PostsCtrl'
+app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('home', {
+		  url: '/home',
+		  templateUrl: '/home.html',
+		  controller: 'MainCtrl',
+		  resolve: {
+		    postPromise: ['posts', function(posts){
+		      return posts.getAll();
+		    }]
+		  }
 		});
-	}]);
+
+		.state('posts', {
+		  url: '/posts/{id}',
+		  templateUrl: '/posts.html',
+		  controller: 'PostsCtrl',
+		  resolve: {
+		    post: ['$stateParams', 'posts', function($stateParams, posts) {
+		      return posts.get($stateParams.id);
+		    }]
+		  }
+		});
+	$urlRouterProvider.otherwise('home');
+}]);
